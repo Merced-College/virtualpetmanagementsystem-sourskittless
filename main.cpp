930f4617fc632e5e12dddad8d537b665579bedeb
sourskittless <sjimenezjr05@gmail.com>
2026-02-12 13:05:45 -0800

    Replace Pet instances with Dog instances in main

--- main.cpp
+++ main.cpp
@@ -2,20 +2,21 @@
 #include "Pet.h"
 
 int main() {
-    Pet pet1("Rex", "Dog", 3, 6);
-    Pet pet2("Whiskers", "Cat", 2, 4);
+    Dog dog1("Rex", "Dog", 3, 6);
+    Dog dog2("Sam", "Dog", 2, 4);
 
-    std::cout << "Pet 1 Info:\n";
-    pet1.printInfo();
+    std::cout << "Dog 1 Info:\n";
+    dog1.printInfo();
 
-    std::cout << "\nPet 2 Info:\n";
-    pet2.printInfo();
+    std::cout << "\nDog 2 Info:\n";
+    dog2.printInfo();
 
     std::cout << "\nComparing pets by name: ";
-    int result = pet1.compareTo(pet2);
-    if (result < 0) std::cout << pet1.getName() << " comes before " << pet2.getName() << "\n";
-    else if (result > 0) std::cout << pet1.getName() << " comes after " << pet2.getName() << "\n";
+    int result = dog1.compareTo(dog2);
+    if (result < 0) std::cout << dog1.getName() << " comes before " << dog2.getName() << "\n";
+    else if (result > 0) std::cout << dog1.getName() << " comes after " << dog2.getName() << "\n";
     else std::cout << "Both pets have the same name.\n";
+    
 
     return 0;
 }
